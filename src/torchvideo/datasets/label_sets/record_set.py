828bb27526e4786600ad8f561fb11a7bf65d9896
Alex Andonian <alexandonian@gmail.com>
2019-11-05 05:31:02 -0500

    Add FrameRecordSet

--- src/torchvideo/datasets/label_sets/record_set.py
+++ src/torchvideo/datasets/label_sets/record_set.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from collections import namedtuple
 
-VideoRecord = namedtuple('VideoRecord', ['path', 'num_frames', 'label'])
+VideoRecord = namedtuple('VideoRecord', ['path', 'label'])
+FrameFolderRecord = namedtuple('FrameFolderRecord', ['path', 'num_frames', 'label'])
 
 
 class RecordSet:
@@ -11,11 +12,26 @@ class RecordSet:
         self.filename = filename
         with open(filename) as f:
             for line in f:
-                path, num_frames, label = line.strip().split(sep)
-                self.records.append(VideoRecord(path + '.mp4', int(num_frames), int(label)))
+                path, label = line.strip().split(sep)
+                self.records.append(VideoRecord(path, int(label)))
 
     def __getitem__(self, idx: int) -> VideoRecord:
         return self.records[idx]
 
     def __len__(self):
         return len(self.records)
+
+
+class FrameRecordSet(RecordSet):
+
+    def __init__(self, filename, sep: Optional[str] = ' '):
+        self.records = []
+        self.filename = filename
+        with open(filename) as f:
+            for line in f:
+                path, num_frames, label = line.strip().split(sep)
+                self.records.append(FrameFolderRecord(path + '.mp4', int(num_frames), int(label)))
+
+    def __getitem__(self, idx: int) -> FrameFolderRecord:
+        return self.records[idx]
+
