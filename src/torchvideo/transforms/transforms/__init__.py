ddcadd28c72091428f9bc7d130c6498ee404709c
Alex Andonian <alexandonian@gmail.com>
2020-01-22 04:34:31 -0500

    Add RandomRotationVideo transform

--- src/torchvideo/transforms/transforms/__init__.py
+++ src/torchvideo/transforms/transforms/__init__.py
@@ -16,6 +16,7 @@ __all__ = [
     "TimeToChannel",
     "RandomResizedCropVideo",
     "ImageShape",
+    "RandomRotationVideo"
 ]
 
 from .center_crop_video import CenterCropVideo
@@ -27,17 +28,12 @@ from .ndarray_to_pil_video import NDArrayToPILVideo
 from .normalize_video import NormalizeVideo
 from .pil_video_to_tensor import PILVideoToTensor
 from .random_crop_video import RandomCropVideo
-from .random_resized_crop_video import RandomResizedCropVideo
 from .random_horizontal_flip_video import RandomHorizontalFlipVideo
+from .random_resized_crop_video import RandomResizedCropVideo
+from .random_rotation import RandomRotationVideo
 from .resize_video import ResizeVideo
 from .time_apply import TimeApply
 from .time_to_channel import TimeToChannel
-from .transform import Transform, StatelessTransform, FramesAndParams
-from .transform import Transform
-from .types import (
-    ImageSizeParam,
-    ImageShape,
-    InputFramesType,
-    OutputFramesType,
-    ParamsType,
-)
+from .transform import FramesAndParams, StatelessTransform, Transform
+from .types import (ImageShape, ImageSizeParam, InputFramesType,
+                    OutputFramesType, ParamsType)
